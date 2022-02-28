9231b7383b86c22627c7bd02465ee2c5bf573fa0
Remox <giwathankgod@gmail.com>
2022-02-28 23:41:12 +0100

    'settings.py'

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=dau+s9e*0rem!&kh#vx$oum8z4)eev6+0sfn_=+spy#-k0%(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['remox-hosting.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['boomie.herokuapp.com','127.0.0.1']
 
 
 # Application definition
