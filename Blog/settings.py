893e18a8f70de65992e00420d89bb2838177b996
Remox <giwathankgod@gmail.com>
2022-02-28 23:58:04 +0100

    new hosting app

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=dau+s9e*0rem!&kh#vx$oum8z4)eev6+0sfn_=+spy#-k0%(r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['boomie.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['boomie.herokuapp.com','remox-hosting.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 
